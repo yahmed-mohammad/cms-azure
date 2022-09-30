9d7a72e819c6cd93065658e760cda39ae59f58eb
Mohammad Ahmed <yahmed.mohammad@gmail.com>
2022-09-30 12:25:42 +0530

    Code Changes

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,12 +10,13 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
-app.logger.setLevel(logging.WARNING)
-handler =  logging.StreamHandler()
-handler.setLevel(logging.WARNING)
-app.logger.addHandler(handler)
 
+logging.basicConfig(
+    # filename='./flask-app.log',
+    # filemode='a',
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 Session(app)
 db = SQLAlchemy(app)
